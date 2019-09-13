1c0343328e8aebccf87f4bded38e3d940284b61c
Bernhard M. Wiedemann <bwiedemann@suse.de>
2019-09-13 13:17:28 +0000

    restructure files
    to remain below github limit of 1000 entries

---
+++ local.pm
@@ -0,0 +1,30 @@
+#################################################################
+# local.pm
+#
+# Local Configuration for LaTeX2HTML
+#
+# This file is created automatically. Do not edit!
+#
+#################################################################
+
+package main;
+
+### start pstoimg configuration ###
+$GS_LIB = '   . ; /usr/share/ghostscript/5.50 ; /usr/share/ghostscript/fonts'; # Inserted by configure-pstoimg
+$GS_DEVICE = 'ppmraw'; # Inserted by configure-pstoimg
+$GS = '/usr/bin/gs'; # Inserted by configure-pstoimg
+$PNMCAT = '/usr/X11R6/bin/pnmcat'; # Inserted by configure-pstoimg
+$PNMFILE = '/usr/X11R6/bin/pnmfile'; # Inserted by configure-pstoimg
+$PNMTOPNG = '/usr/X11R6/bin/pnmtopng'; # Inserted by configure-pstoimg
+$PBMMAKE = '/usr/X11R6/bin/pbmmake'; # Inserted by configure-pstoimg
+$GSLANDSCAPE = '/usr/share/ghostscript/5.50/landscap.ps'; # Inserted by configure-pstoimg
+$PPMQUANT = '/usr/X11R6/bin/ppmquant'; # Inserted by configure-pstoimg
+$PNMFLIP = '/usr/X11R6/bin/pnmflip'; # Inserted by configure-pstoimg
+$bg_color = '\#bfbfbf'; # Inserted by configure-pstoimg
+$white_color = '\#ffffff'; # Inserted by configure-pstoimg
+$PNMCROP = '/usr/X11R6/bin/pnmcrop'; # Inserted by configure-pstoimg
+
+### end pstoimg configuration ###
+
+1;
+
