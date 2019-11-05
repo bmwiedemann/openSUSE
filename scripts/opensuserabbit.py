c36f33d82894c239da2718b11ae54e3f5b2fd608
Bernhard M. Wiedemann <bwiedemann@suse.de>
2019-11-05 10:16:37 +0100

    Add rabbitmq handling scripts

---
+++ scripts/opensuserabbit.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python -u
+import pika
+import sys
+
+# or amqps://suse:suse@rabbit.suse.de
+url = "amqps://opensuse:opensuse@rabbit.opensuse.org"
+if len(sys.argv) >= 2:
+    url = sys.argv[1]
+prefix= "opensuse"
+if len(sys.argv) >= 3:
+    prefix = sys.argv[2]
+connection = pika.BlockingConnection(pika.URLParameters(url))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='pubsub',
+                         passive=True, durable=True)
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='pubsub', queue=queue_name, routing_key=prefix+'.obs.package.commit')
+#channel.queue_bind(exchange='pubsub', queue=queue_name, routing_key=prefix+'.obs.request.state_change')
+
+def callback(ch, method, properties, body):
+    #if method.routing_key == "opensuse.obs.request.create":
+        print(body)
+        sys.stdout.flush()
+
+channel.basic_consume(callback,
+                      queue=queue_name,
+                      no_ack=True)
+
+channel.start_consuming()
---
+++ scripts/rabbithandle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# use opensuserabbit.py | rabbithandle.py
+import json
+import subprocess
+import sys
+
+obsbase='https://build.opensuse.org'
+outdir='packages'
+
+def pkgmap(p):
+     first=p[0]
+     if p.startswith("lib"):
+         first=p[0:4]
+     return outdir+"/"+first.lower()+"/"+p
+
+for line in sys.stdin:
+    change = json.loads(line)
+    if not 'package' in change:
+        continue
+    if change['project'] != 'openSUSE:Factory':
+        continue
+    package = change['package']
+    info = 'Update '+package+' to rev '+change['rev']
+    if 'requestid' in change:
+        info += ' via SR '+change['requestid']+"\n\n"+obsbase+'/request/show/'+change['requestid']+"\n"
+    else:
+        info += "\n\n"+obsbase+'/package/rdiff/'+change['project']+'/'+package+'?linkrev=base&rev='+change['rev']+"\n";
+    info += 'by user '+change['user']+"\n"
+    if 'comment' in change:
+        info += change['comment']
+    info += '\n'
+    print(info);
+    subprocess.call(["tail", "-10", "/mounts/work/SRC/openSUSE:Factory/"+package+"/.rev"], shell=False);
+    subprocess.call(["scripts/syncone", package], shell=False)
+    subprocess.call(["git", "add", pkgmap(package)], shell=False)
+    process = subprocess.Popen(["git", "commit", "-F", "-"], stdin=subprocess.PIPE)
+    process.communicate(info.encode('utf-8'))
