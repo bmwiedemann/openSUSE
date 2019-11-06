daa5b2229dfb43912ccbe6fcaf427c8140d69a14
Bernhard M. Wiedemann <bwiedemann@suse.de>
2019-11-06 13:01:06 +0000

    Update ceph-csi-image to rev 4 via SR 745143
    https://build.opensuse.org/request/show/745143
    by user dimstar_suse

---
+++ packages/c/ceph-csi-image/pre-checkin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Generate the kiwi file for the given target.
+
+image="ceph-csi-image"
+history="$(sed -n "s/^-[ ]//p" <${image}.changes | head -1)"
+target=${1:-tumbleweed}
+if [ ! -e ${target}.xml ]; then
+  echo "Unknown target: ${target}.xml must exist!"
+  exit 1
+fi
+xsltproc --stringparam history "$history" "${image}.xsl" "${target}.xml" > "${image}.kiwi"
+
