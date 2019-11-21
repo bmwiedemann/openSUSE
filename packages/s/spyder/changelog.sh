821ac16c93da5e8848fc698c33ee7e243394ae7f
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2019-11-21 12:30:45 +0000

    update 2019-11-21 12:30

--- packages/s/spyder/changelog.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xclip -o | egrep -v "(^[-\s]*$|^In this release )" | sed -E 's/^### ([A-Za-z ]+)/ - \1:/' | sed -E 's/^\* /  - /' | sed -E 's/^## Version ([0-9.]+) \([0-9-]+\)/- update to version \1:/' | xclip -i
