cebb3d26c1d81fc139268bb013cf4193fb22bb89
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2019-12-23 22:00:15 +0000

    Update gnome-menus-branding to rev 1 via SR 758997
    https://build.opensuse.org/request/show/758997
    by user dimstar_suse
    Submit

---
+++ packages/g/gnome-menus-branding/pre_checkin.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+NAME=gnome-menus
+
+if ! test -f ${NAME}-branding.spec.in ; then
+    exit
+fi
+
+for variant in openSUSE SLED ; do
+    cp ${NAME}-branding.spec.in ${NAME}-branding-${variant}.spec
+    cp ${NAME}-branding.changes.in ${NAME}-branding-${variant}.changes
+    sed -i "s/%{branding_name}/${variant}/g" ${NAME}-branding-${variant}.spec
+    sed -i "s/\(%define *build_${variant} *\)0/\11/" ${NAME}-branding-${variant}.spec
+    sed -i "
+    /^Name: *${NAME}-branding-${variant}/i \
+# Do not edit this auto generated file! Edit ${NAME}-branding.spec.in.
+" ${NAME}-branding-${variant}.spec
+done
