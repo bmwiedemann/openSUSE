340d9953afb178ad100f390843d43bdcf16fa0bd
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2020-02-25 15:09:08 +0000

    Update openSUSE-MicroOS to rev 19 via SR 778822
    https://build.opensuse.org/request/show/778822
    by user favogt + dimstar_suse

--- packages/o/openSUSE-MicroOS/config.sh
+++ packages/o/openSUSE-MicroOS/config.sh
@@ -174,6 +174,9 @@ if [[ "$kiwi_profiles" == *"RaspberryPi"* ]]; then
 	# Add necessary kernel modules to initrd (will disappear with bsc#1084272)
 	echo 'add_drivers+=" bcm2835_dma dwc2 "' > /etc/dracut.conf.d/raspberrypi_modules.conf
 
+	# Add necessary kernel modules to initrd (will disappear with boo#1162669)
+	echo 'add_drivers+=" pcie-brcmstb "' >> /etc/dracut.conf.d/raspberrypi_modules.conf
+
 	# Work around network issues
   	cat > /etc/modprobe.d/50-rpi3.conf <<-EOF
 		# Prevent too many page allocations (bsc#1012449)
