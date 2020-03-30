ed71a3004db3b2ab12903db84487a1b9e9573b3d
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2020-03-30 20:51:36 +0000

    Update sysuser-tools to rev 9 via SR 788122
    https://build.opensuse.org/request/show/788122
    by user kukuk + dimstar_suse

--- packages/s/sysuser-tools/sysusers2shadow.sh
+++ packages/s/sysuser-tools/sysusers2shadow.sh
@@ -1,96 +1,90 @@
-#!/bin/bash
+#!/bin/sh
+set -eu
+
+# Print the command and run it
+run() {
+	echo "$@"
+	"$@"
+}
+
+# Absolute path to busybox, if found
+busybox=
+for i in /bin/busybox /usr/bin/busybox; do [ -x "$i" ] && busybox=$i; done
 
 while read LINE
 do
-    case "$LINE" in
+	eval set -- $LINE
+	case "${1-}" in
 	\#*|"")
-	    ;;
-	g*)
-            eval arr=( $LINE )
-            ARGUMENTS="${arr[1]}"
-            if [ ! -z "${arr[2]}" -a "${arr[2]}" != "-" ]; then
-                ARGUMENTS="-g ${arr[2]} $ARGUMENTS"
-	    fi
-	    if [ -x /usr/sbin/groupadd ]; then
-		echo "groupadd -r $ARGUMENTS"
-		/usr/bin/getent group "${arr[1]}" >> /dev/null || /usr/sbin/groupadd -r $ARGUMENTS || exit $?
-	    elif [ -x /usr/bin/busybox ]; then
-		echo "addgroup -S $ARGUMENTS"
-		/usr/bin/getent group "${arr[1]}" >> /dev/null || /usr/bin/busybox addgroup -S $ARGUMENTS || exit $?
-	    elif [ -x /bin/busybox ]; then
-		echo "addgroup -S $ARGUMENTS"
-		/usr/bin/getent group "${arr[1]}" >> /dev/null || /bin/busybox addgroup -S $ARGUMENTS || exit $?
-	    else
-		echo "ERROR: neither groupadd nor busybox found!"
-		exit 1
-	    fi
-	    ;;
-	u*)
-            eval arr=( $LINE )
-	    ARGUMENTS="${arr[1]}"
-	    if [ ! -z "${arr[2]}" -a "${arr[2]}" != "-" ]; then
-		ARGUMENTS="-u ${arr[2]} $ARGUMENTS"
-	    fi
-	    if [ ! -z "${arr[4]}" -a "${arr[4]}" != "-" ]; then
-		ARGUMENTS="-d ${arr[4]} $ARGUMENTS"
-	    else
-		ARGUMENTS="-d / $ARGUMENTS"
-	    fi
-	    if [ -x /usr/sbin/useradd ]; then
-		# this is useradd/shadow specific
-		/usr/bin/getent group ${arr[1]} >> /dev/null
-		if [ $? -eq 0 ]; then
-              	    ARGUMENTS="-g ${arr[1]} $ARGUMENTS"
+		;;
+	g)
+		shift
+		ARGUMENTS="$1"
+		if [ -n "${2-}" ] && [ "$2" != "-" ]; then
+			ARGUMENTS="-g $2 $ARGUMENTS"
+		fi
+
+		if ! /usr/bin/getent group "$1" >> /dev/null; then
+			if [ -x "/usr/sbin/groupadd" ]; then
+				run /usr/sbin/groupadd -r $ARGUMENTS
+			elif [ -x "$busybox" ]; then
+				run $busybox addgroup -S $ARGUMENTS
+			else
+				echo "ERROR: neither groupadd nor busybox found!"
+				exit 1
+			fi
+		fi
+		;;
+	u)
+		shift
+		ARGUMENTS="$1"
+		if [ -n "${2-}" ] && [ "$2" != "-" ]; then
+			ARGUMENTS="-u $2 $ARGUMENTS"
+		fi
+		homedir="/" # If null, empty or '-'
+		if [ "${4:--}" != "-" ]; then
+			homedir="$4"
+		fi
+
+		if [ -x /usr/sbin/useradd ]; then
+			if ! /usr/bin/getent passwd "$1" >> /dev/null; then
+				# this is useradd/shadow specific
+				if /usr/bin/getent group "$1" >> /dev/null; then
+					ARGUMENTS="-g $1 $ARGUMENTS"
+				else
+					ARGUMENTS="-U $ARGUMENTS"
+				fi
+			
+				run /usr/sbin/useradd -r -s /sbin/nologin -c "$3" -d "${homedir}" $ARGUMENTS
+			fi
+		elif [ -x "$busybox" ]; then
+			/usr/bin/getent group "$1" >> /dev/null || $busybox addgroup "$1"
+
+			if ! /usr/bin/getent passwd "$1" >> /dev/null; then
+				run $busybox adduser -S -H -s /sbin/nologin -g "$3" -G "$1" -h "${homedir}" $ARGUMENTS
+			fi
 		else
-		    ARGUMENTS="-U $ARGUMENTS"
+			echo "ERROR: neither useradd nor busybox found!"
+			exit 1
 		fi
-		echo "useradd -r -s /sbin/nologin -c \"${arr[3]}\" $ARGUMENTS"
-		/usr/bin/getent passwd ${arr[1]} >> /dev/null || /usr/sbin/useradd -r -s /sbin/nologin -c "${arr[3]}" $ARGUMENTS || exit $?
-	    elif [ -x /usr/bin/busybox ]; then
-		/usr/bin/getent group ${arr[1]} >> /dev/null
-		if [ $? -ne 0 ]; then
-		    /usr/bin/busybox addgroup ${arr[1]}
+	    ;;
+	m)
+		shift
+		if [ -x /usr/sbin/usermod ] ; then
+				run /usr/sbin/usermod -a -G $2 $1
+		elif [ -x "$busybox" ]; then
+				run $busybox addgroup $1 $2
+		else
+				echo "ERROR: neither usermod nor busybox found!"
+				exit 1
 		fi
-              	ARGUMENTS="-G ${arr[1]} $ARGUMENTS"
-		ARGUMENTS=`echo $ARGUMENTS | sed -e 's|-d|-h|g' -e 's|-g|-G|g'`
-		echo "adduser -S -s /sbin/nologin -g \"${arr[3]}\" $ARGUMENTS"
-		/usr/bin/getent passwd ${arr[1]} >> /dev/null || /usr/bin/busybox adduser -S -s /sbin/nologin -g "${arr[3]}" $ARGUMENTS || exit $?
-	    elif [ -x /bin/busybox ]; then
-		/usr/bin/getent group ${arr[1]} >> /dev/null
-		if [ $? -ne 0 ]; then
-		    /bin/busybox addgroup ${arr[1]}
-		fi
-              	ARGUMENTS="-G ${arr[1]} $ARGUMENTS"
-		ARGUMENTS=`echo $ARGUMENTS | sed -e 's|-d|-h|g' -e 's|-g|-G|g'`
-		echo "adduser -S -s /sbin/nologin -g \"${arr[3]}\" $ARGUMENTS"
-		/usr/bin/getent passwd ${arr[1]} >> /dev/null || /bin/busybox adduser -S -s /sbin/nologin -g "${arr[3]}" $ARGUMENTS || exit $?
-	    else
-		echo "ERROR: neither useradd nor busybox found!"
-		exit 1
-	    fi
 	    ;;
-	m*)
-            eval arr=( $LINE )
-	    if [ -x /usr/sbin/usermod ] ; then
-		echo "usermod -a -G ${arr[2]} ${arr[1]}"
-		/usr/sbin/usermod -a -G ${arr[2]} ${arr[1]} || exit $?
-	    elif [ -x /usr/bin/busybox ]; then
-		echo "addgroup ${arr[1]} ${arr[2]}"
-		/usr/bin/busybox addgroup ${arr[1]} ${arr[2]} || exit $?
-	    elif [ -x /bin/busybox ]; then
-		echo "addgroup ${arr[1]} ${arr[2]}"
-		/bin/busybox addgroup ${arr[1]} ${arr[2]} || exit $?
-	    else
-		echo "ERROR: neither usermod nor busybox found!"
-		exit 1
-	    fi
-	    ;;
-	r*)
-	    echo "range option ignored: \"$LINE\""
-	    ;;
+	r)
+		echo "range option ignored: \"$LINE\""
+		;;
 	*)
 	    echo "Syntax Error: \"$LINE\""
 	    exit 1
 	    ;;
-    esac
+	esac
 done
