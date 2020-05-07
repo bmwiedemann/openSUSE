c313a2f6b92d179eee5a83b238bfaa1a35adb4f7
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2020-05-07 10:31:19 +0000

    update 2020-05-07 10:31

--- scripts/buildpkg.sh
+++ scripts/buildpkg.sh
@@ -9,4 +9,19 @@ pkg=$(basename $(pwd))
 mkdir -p .osc
 echo "$project" > .osc/_project
 echo "$pkg" > .osc/_package
+prjconf=.osc/_buildconfig-standard-x86_64
+cat > $prjconf <<EOPRJCONF
+%define _project openSUSE:Factory
+%define _repository standard
+Macros:
+%vendor obs://build.opensuse.org/openSUSE:Factory
+%_download_url http://download.opensuse.org/repositories
+%_project openSUSE:Factory
+%distribution openSUSE:Factory
+%_project openSUSE:Factory
+%_repository standard
+:Macros
+EOPRJCONF
+cat ../../_/_project/_config >> $prjconf
+
 osc build --noservice "$@"
