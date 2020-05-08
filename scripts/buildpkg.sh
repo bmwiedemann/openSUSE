8b264f0c762bdbf8ec341ef76b8e654b13864285
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2020-05-08 04:31:01 +0000

    update 2020-05-08 04:31

--- scripts/buildpkg.sh
+++ scripts/buildpkg.sh
@@ -5,11 +5,14 @@ set -x
 project=${project:-`cat ../.project 2>/dev/null`}
 project=${project:-openSUSE:Factory}
 set -e
+repo=${repo:-standard}
+arch=${arch:-x86_64}
 pkg=$(basename $(pwd))
 mkdir -p .osc
 echo "$project" > .osc/_project
 echo "$pkg" > .osc/_package
-prjconf=.osc/_buildconfig-standard-x86_64
+prjconf=.osc/_buildconfig-$repo-$arch
+buildinfo=.osc/_buildinfo-$repo-$arch
 cat > $prjconf <<EOPRJCONF
 %define _project openSUSE:Factory
 %define _repository standard
@@ -23,5 +26,6 @@ Macros:
 :Macros
 EOPRJCONF
 cat ../../_/_project/_config >> $prjconf
+#osc buildinfo > $buildinfo
 
 osc build --noservice "$@"
