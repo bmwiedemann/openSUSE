5ef70bf6729cbf4e0dd439c907d4f7a8bdc813c9
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2020-05-09 17:48:24 +0000

    Update tumbleweed-busybox-image to rev 7 via SR 798585
    https://build.opensuse.org/request/show/798585
    by user kukuk + dimstar_suse
    - Add busybox-sh (was splitted from busybox-ash because of bash)
    
    - Add LICENSE file
    
    - Use busybox-ash instead of busybox-sh
    
    - config.sh: don't create symlinks which we package now
    - list all packages, don't use busybox-links meta package
    
    - Use 'busybox' as 'busybox-container' was dropped
    
    - Convert to use busybox-links
    
    - busybox-container was merged with busybox

--- packages/t/tumbleweed-busybox-image/config.sh
+++ packages/t/tumbleweed-busybox-image/config.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/busybox-container sh
+#!/usr/bin/busybox sh
   
 #======================================
 # Functions...
@@ -12,21 +12,7 @@ test -f /.profile && . /.profile
 #--------------------------------------
 echo "Configure image: [$kiwi_iname]..."
 
-/usr/bin/busybox-container ln -sf busybox-container /usr/bin/busybox
-/usr/bin/busybox ln -sf ../usr/bin/busybox /bin/ln
-/usr/bin/busybox ln -sf ../usr/bin/busybox /bin/rm
-/usr/bin/busybox ln -sf ../usr/bin/busybox /bin/mkdir
-/usr/bin/busybox ln -sf ../usr/bin/busybox /bin/chmod
-/usr/bin/busybox ln -sf busybox /usr/bin/sort
-/usr/bin/busybox ln -sf busybox /usr/bin/uniq
-/usr/bin/busybox ln -sf busybox /usr/bin/install
-/usr/bin/busybox ln -sf busybox /usr/bin/dirname
-/usr/bin/busybox ln -sf busybox /usr/bin/basename
-/usr/bin/busybox-container.install / --symlinks
-/bin/rm linuxrc
-/bin/rm /usr/bin/busybox
-/bin/rm /usr/bin/busybox-container
-/bin/rm /usr/bin/busybox-container.install
-/bin/rm -rf /usr/share/busybox
+/usr/bin/busybox ln -sf busybox /usr/bin/rpm
+/usr/bin/busybox ln -sf busybox /usr/bin/rpm2cpio
 
 exit 0
