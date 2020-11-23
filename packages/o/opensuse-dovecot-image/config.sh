036366cc45e8da90aa3f5fa3654665a0c86b56a3
kukuk <>
2020-11-23 14:44:14 +0000

    Update opensuse-dovecot-image to version 1.0.0 / rev 1 via SR 850100
    https://build.opensuse.org/request/show/850100
    by user kukuk + dimstar_suse
    Initial dovecot image, should be released on registry.opensuse.org

---
+++ packages/o/opensuse-dovecot-image/config.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+  
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+# remove default configuration files
+rm -rf /etc/dovecot/*
