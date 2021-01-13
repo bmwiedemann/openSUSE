ecfa44012d40755e196be6641c99bd8ed43b3431
dimstar_suse <>
2021-01-13 14:37:14 +0000

    Update livecd-openSUSE to rev 134
    https://build.opensuse.org/package/rdiff/openSUSE:Factory/livecd-openSUSE?linkrev=base&rev=134
    by user dimstar_suse
    Drop cypress firmware

--- packages/l/livecd-openSUSE/config.sh
+++ packages/l/livecd-openSUSE/config.sh
@@ -78,7 +78,7 @@ zypper --non-interactive rm yast2-trans-{uk,sv,ru,ja,da,cs,sr,vi} || :
 rm -rf /usr/share/doc/packages/*
 
 # Save more than 200 MiB by removing this, not very useful for lives
-rm -rf /lib/firmware/{liquidio,netronome,qed,mrvl,mellanox,qcom}
+rm -rf /lib/firmware/{liquidio,netronome,qed,mrvl,mellanox,qcom,cypress}
 
 # Add repos from /etc/YaST2/control.xml
 add-yast-repos
