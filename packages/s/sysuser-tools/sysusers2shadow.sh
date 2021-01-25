be92cdb94985e14f9d52947a4f8ca1d990297beb
dimstar_suse <>
2021-01-25 09:30:35 +0000

    Update sysuser-tools to version 3.0 / rev 14
    https://build.opensuse.org/package/rdiff/openSUSE:Factory/sysuser-tools?linkrev=base&rev=14
    by user dimstar_suse
    https://bugzilla.opensuse.org/show_bug.cgi?id=1181328

--- packages/s/sysuser-tools/sysusers2shadow.sh
+++ packages/s/sysuser-tools/sysusers2shadow.sh
@@ -7,98 +7,85 @@ run() {
 	"$@"
 }
 
-if [ -x /usr/bin/systemd-sysusers ]; then
+# Absolute path to busybox, if found
+busybox=
+for i in /bin/busybox /usr/bin/busybox; do [ -x "$i" ] && busybox=$i; done
 
-    # Use systemd-sysusers and let it read the input directly from stdin
-    /usr/bin/systemd-sysusers -
-    # We need to clear the nscd cache, as systemd-sysusers could have
-    # triggered a negative cache entry
-    if [ -x /usr/sbin/nscd ]; then
-	/usr/sbin/nscd -i passwd ||:
-	/usr/sbin/nscd -i group ||:
-    fi
-else
-
-    # Absolute path to busybox, if found
-    busybox=
-    for i in /bin/busybox /usr/bin/busybox; do [ -x "$i" ] && busybox=$i; done
-
-    while read LINE
-    do
+while read LINE
+do
 	# "eval set" to do proper splitting while respecting quotes
 	eval set -- $LINE
 	case "${1-}" in
-	    \#*|"")
-	    ;;
-	    g)
+	\#*|"")
+		;;
+	g)
 		shift
 		ARGUMENTS="$1"
 		if [ -n "${2-}" ] && [ "$2" != "-" ]; then
-		    ARGUMENTS="-g $2 $ARGUMENTS"
+			ARGUMENTS="-g $2 $ARGUMENTS"
 		fi
 
 		if ! /usr/bin/getent group "$1" >> /dev/null; then
-		    if [ -x "/usr/sbin/groupadd" ]; then
-			run /usr/sbin/groupadd -r $ARGUMENTS
-		    elif [ -x "$busybox" ]; then
-			run $busybox addgroup -S $ARGUMENTS
-		    else
-			echo "ERROR: neither groupadd nor busybox found!"
-			exit 1
-		    fi
+			if [ -x "/usr/sbin/groupadd" ]; then
+				run /usr/sbin/groupadd -r $ARGUMENTS
+			elif [ -x "$busybox" ]; then
+				run $busybox addgroup -S $ARGUMENTS
+			else
+				echo "ERROR: neither groupadd nor busybox found!"
+				exit 1
+			fi
 		fi
 		;;
-	    u)
+	u)
 		shift
 		ARGUMENTS="$1"
 		if [ -n "${2-}" ] && [ "$2" != "-" ]; then
-		    ARGUMENTS="-u $2 $ARGUMENTS"
+			ARGUMENTS="-u $2 $ARGUMENTS"
 		fi
 		homedir="/" # If null, empty or '-'
 		if [ "${4:--}" != "-" ]; then
-		    homedir="$4"
+			homedir="$4"
 		fi
 
 		if [ -x /usr/sbin/useradd ]; then
-		    if ! /usr/bin/getent passwd "$1" >> /dev/null; then
-			# this is useradd/shadow specific
-			if /usr/bin/getent group "$1" >> /dev/null; then
-			    ARGUMENTS="-g $1 $ARGUMENTS"
-			else
-			    ARGUMENTS="-U $ARGUMENTS"
+			if ! /usr/bin/getent passwd "$1" >> /dev/null; then
+				# this is useradd/shadow specific
+				if /usr/bin/getent group "$1" >> /dev/null; then
+					ARGUMENTS="-g $1 $ARGUMENTS"
+				else
+					ARGUMENTS="-U $ARGUMENTS"
+				fi
+			
+				run /usr/sbin/useradd -r -s /sbin/nologin -c "$3" -d "${homedir}" $ARGUMENTS
 			fi
-
-			run /usr/sbin/useradd -r -s /sbin/nologin -c "$3" -d "${homedir}" $ARGUMENTS
-		    fi
 		elif [ -x "$busybox" ]; then
-		    /usr/bin/getent group "$1" >> /dev/null || $busybox addgroup -S "$1"
+			/usr/bin/getent group "$1" >> /dev/null || $busybox addgroup -S "$1"
 
-		    if ! /usr/bin/getent passwd "$1" >> /dev/null; then
-			run $busybox adduser -S -H -s /sbin/nologin -g "$3" -G "$1" -h "${homedir}" $ARGUMENTS
-		    fi
+			if ! /usr/bin/getent passwd "$1" >> /dev/null; then
+				run $busybox adduser -S -H -s /sbin/nologin -g "$3" -G "$1" -h "${homedir}" $ARGUMENTS
+			fi
 		else
-		    echo "ERROR: neither useradd nor busybox found!"
-		    exit 1
+			echo "ERROR: neither useradd nor busybox found!"
+			exit 1
 		fi
-		;;
-	    m)
+	    ;;
+	m)
 		shift
 		if [ -x /usr/sbin/usermod ] ; then
-		    run /usr/sbin/usermod -a -G $2 $1
+				run /usr/sbin/usermod -a -G $2 $1
 		elif [ -x "$busybox" ]; then
-		    run $busybox addgroup $1 $2
+				run $busybox addgroup $1 $2
 		else
-		    echo "ERROR: neither usermod nor busybox found!"
-		    exit 1
+				echo "ERROR: neither usermod nor busybox found!"
+				exit 1
 		fi
-		;;
-	    r)
+	    ;;
+	r)
 		echo "range option ignored: \"$LINE\""
 		;;
-	    *)
-		echo "Syntax Error: \"$LINE\""
-		exit 1
-		;;
+	*)
+	    echo "Syntax Error: \"$LINE\""
+	    exit 1
+	    ;;
 	esac
-    done
-fi
+done
