06078a9181fc2737c650dbbdfc7ebfcae1d0f5fa
hgraeber <>
2021-03-03 17:33:54 +0000

    Update wsdd to version 0.6.4 / rev 1 via SR 876006
    https://build.opensuse.org/request/show/876006
    by user hgraeber + RBrownSUSE
    Updated user creation and removed use of /run/sysconfig

---
+++ packages/w/wsdd/wsdd-init.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+umask 0644
+
+WSDD_CONFIG=/etc/sysconfig/wsdd
+if test -r "${WSDD_CONFIG}"; then
+  . "${WSDD_CONFIG}"
+fi
+
+if [ "${WSDD_DOMAIN}" != "" ]; then
+  WSDD_DOMAIN="-d \"${WSDD_DOMAIN}\""
+elif [ "${WSDD_WORKGROUP}" != "" ]; then
+  WSDD_DOMAIN="-w \"${WSDD_WORKGROUP}\""
+fi
+
+if [ "${WSDD_HOSTNAME}" != "" ]; then
+  WSDD_HOSTNAME="-n \"${WSDD_HOSTNAME}\""
+fi
+
+WSDD_INTERFACE_ARGS=""
+if [ "${WSDD_INTERFACES}" != "" ]; then
+  for intf in "${WSDD_INTERFACES[@]}"; do
+    echo ${intf}
+    WSDD_INTERFACE_ARGS="${WSDD_INTERFACE_ARGS} -i \"${intf}\""
+  done
+fi
+
+echo "WSDD_ARGS=${WSDD_HOSTNAME} ${WSDD_DOMAIN} ${WSDD_INTERFACE_ARGS} \
+	${WSDD_ARGS}" >/run/wsdd/env-vars
