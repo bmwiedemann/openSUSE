e66a00bc5002a4a31a67ebee18aec4612b960fd2
dimstar <>
2021-04-24 21:07:01 +0000

    Update nautilus to version 40.0 / rev 185 via SR 887506
    https://build.opensuse.org/request/show/887506
    by user dimstar + dimstar_suse
    - Update set_trusted.sh: Use the right value in gio command
      (bsc#1185026). (forwarded request 887211 from xiaoguang_wang)

--- set_trusted.sh
+++ set_trusted.sh
@@ -4,7 +4,7 @@ DIR=$HOME/Desktop
 
 for f in $(ls $DIR)
 do
-    gio set -t string $DIR/$f "metadata::trusted" yes
+    gio set -t string $DIR/$f "metadata::trusted" true
 done
 
 if [ -f $HOME/.config/autostart/set_trusted.desktop ]; then
