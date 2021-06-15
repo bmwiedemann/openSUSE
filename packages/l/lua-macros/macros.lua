8111b8dcc3f612b82a6624dd5868e19cca2f5204
mcepl <>
2021-06-15 14:36:56 +0000

    Update lua-macros to version 20170611 / rev 6 via SR 899445
    https://build.opensuse.org/request/show/899445
    by user mcepl + dimstar_suse

--- packages/l/lua-macros/macros.lua
+++ packages/l/lua-macros/macros.lua
@@ -2,7 +2,7 @@
 
 # The major.minor version of Lua
 %lua_version %(lua -e 'print(_VERSION)' | cut -d ' ' -f 2)
-%lua_version_nodots %(perl -e "print %{lua_version}*10")
+%lua_version_nodots %(lua -e 'print(math.floor((%{lua_version}*10)))')
 
 # compiled modules should go here
 %lua_archdir %{_libdir}/lua/%{lua_version}
@@ -15,14 +15,20 @@
 
 # Lua default version
 # -e: Exclude lua prefix
-%lua_provides(e) \
+# -n: Specify name
+%lua_provides(en:) \
 %if "%{flavor}" == "lua54" \
+%if 0%{?-n:1} \
+Provides: %{-n*} = %{version}-%{release} \
+Obsoletes: %{-n*} < %{version}-%{release} \
+%else \
 %if 0%{?-e:1} \
 Provides: %{mod_name} = %{version}-%{release} \
 Obsoletes: %{mod_name} < %{version}-%{release} \
 %else \
 Provides: lua-%{mod_name} = %{version}-%{release} \
 Obsoletes: lua-%{mod_name} < %{version}-%{release} \
+%endif \
 %endif \
 %endif \
 %{nil}
