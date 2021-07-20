867c6497ebfbc53930b842124ae232788e16cb25
pnawracay <>
2021-07-20 13:39:39 +0000

    Update ceph-grafana-image to version 1.0.0 / rev 3 via SR 907261
    https://build.opensuse.org/request/show/907261
    by user pnawracay + dimstar_suse

--- packages/c/ceph-grafana-image/config.sh
+++ packages/c/ceph-grafana-image/config.sh
@@ -32,19 +32,9 @@ echo "Configure image: [$kiwi_iname]..."
 suseSetupProduct
 
 #======================================
-# SuSEconfig
-#--------------------------------------
-suseConfig
-
-#======================================
 # Import repositories' keys
 #--------------------------------------
 suseImportBuildKey
-
-#======================================
-# Umount kernel filesystems
-#--------------------------------------
-baseCleanMount
 
 #======================================
 # Disable recommends
@@ -55,12 +45,3 @@ sed -i 's/.*installRecommends.*/installRecommends = no/g' /etc/zypp/zypper.conf
 # Remove locale files
 #--------------------------------------
 (cd /usr/share/locale && find -name '*.mo' | xargs rm)
-
-#======================================
-# adjust lvm conf for ceph-colume
-#--------------------------------------
-sed -i 's/udev_sync = 1/udev_sync = 0/g' /etc/lvm/lvm.conf && \
-sed -i 's/udev_rules = 1/udev_rules = 0/g' /etc/lvm/lvm.conf && \
-sed -i 's/obtain_device_list_from_udev = 1/obtain_device_list_from_udev = 0/g' /etc/lvm/lvm.conf
-
-exit 0
