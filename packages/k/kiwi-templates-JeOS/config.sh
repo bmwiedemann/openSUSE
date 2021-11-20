3c181b952695daacc4b4cff31bed12f57c435a13
gmoro <>
2021-11-20 01:38:23 +0000

    Update kiwi-templates-JeOS to version 84.87 / rev 27 via SR 931917
    https://build.opensuse.org/request/show/931917
    by user gmoro + dimstar_suse

--- packages/k/kiwi-templates-JeOS/config.sh
+++ packages/k/kiwi-templates-JeOS/config.sh
@@ -98,7 +98,8 @@ if [ "${kiwi_btrfs_root_is_snapshot-false}" = 'true' ]; then
         # we can't call snapper here as the .snapshots subvolume
         # already exists and snapper create-config doesn't like
         # that.
-        cp /etc/snapper/config-templates/default /etc/snapper/configs/root
+        cp /etc/snapper/config-templates/default /etc/snapper/configs/root \
+		|| cp /usr/share/snapper/config-templates/default /etc/snapper/configs/root
         # Change configuration to match SLES12-SP1 values
         sed -i -e '/^TIMELINE_CREATE=/s/yes/no/' /etc/snapper/configs/root
         sed -i -e '/^NUMBER_LIMIT=/s/50/10/'     /etc/snapper/configs/root
