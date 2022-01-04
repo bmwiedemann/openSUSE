f2eb51f9ec6fea6e22b85b3816d7e3c264b661c4
vulyanov <>
2022-01-04 20:03:10 +0000

    Update virt-libguestfs-tools-container to rev 2 via SR 943762
    https://build.opensuse.org/request/show/943762
    by user vulyanov + dimstar_suse

--- packages/v/virt-libguestfs-tools-container/entrypoint.sh
+++ packages/v/virt-libguestfs-tools-container/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -xe
 
-# KubeVirt will provide LIBGUESTFS_PATH via the pod environemnt. Unset it so the
-# default search paths are used.
-unset LIBGUESTFS_PATH
+#KubeVirt provides LIBGUESTFS_PATH via the pod environemnt.
+DIR=/usr/local/lib/guestfs
+LIBGUESTFS_VERSION=$(rpm -q --queryformat=%{version} libguestfs)
+LIBGUESTFS_APPLIANCE=/appliance-${LIBGUESTFS_VERSION}.tar.xz
+
+mkdir -p ${DIR}
+tar -Jxf ${LIBGUESTFS_APPLIANCE} -C ${DIR}
+touch ${DIR}/done
 
 /bin/bash
