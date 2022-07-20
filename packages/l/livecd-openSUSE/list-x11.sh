68c11176252ee431bcbdbb0210516d3837eedb38
favogt <>
2022-07-20 10:04:12 +0000

    Update livecd-openSUSE to rev 183 via SR 990373
    https://build.opensuse.org/request/show/990373
    by user favogt + RBrownFactory

--- packages/l/livecd-openSUSE/list-x11.sh
+++ packages/l/livecd-openSUSE/list-x11.sh
@@ -74,6 +74,7 @@ buildignore groff
 buildignore libqt5-qttranslations
 buildignore plymouth
 buildignore 7zip
+buildignore bind-utils
 
 # tumbler -> libgepup uses libwebkit2gtk-4_0-37, while epiphany uses libwebkit2gtk-4_1-0.
 # We can't have both, too big.
