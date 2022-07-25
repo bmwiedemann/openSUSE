fba6d844abf59704c44c9d0de9a9810e12ebfae2
favogt <>
2022-07-25 14:59:46 +0000

    Update micro-image to version 15.Tumbleweed.0 / rev 1 via SR 990265
    https://build.opensuse.org/request/show/990265
    by user favogt + favogt_factory
    Next try

---
+++ packages/m/micro-image/config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: (c) 2022 SUSE LLC
+
+set -euo pipefail
+
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+echo "Configure image: [$kiwi_iname]..."
+
+#============================================
+# Import repositories' keys if rpm is present
+#--------------------------------------------
+if command -v rpm > /dev/null; then
+    suseImportBuildKey
+fi
+
+
+
+
+#=======================================
+# Clean up after zypper if it is present
+#---------------------------------------
+if command -v zypper > /dev/null; then
+    zypper -n clean
+fi
+
+rm -rf /var/log/zypp
+
+exit 0
