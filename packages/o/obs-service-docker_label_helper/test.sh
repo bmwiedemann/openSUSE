08c7c8c9c1a4c35c2463a7250ca3b7a909157b61
favogt <>
2022-07-25 14:59:50 +0000

    Update obs-service-docker_label_helper to version 0.0 / rev 4 via SR 990549
    https://build.opensuse.org/request/show/990549
    by user favogt + favogt_factory
    - Avoid mangling whitespace by using gsub instead of assigning to
      fields

--- packages/o/obs-service-docker_label_helper/test.sh
+++ packages/o/obs-service-docker_label_helper/test.sh
@@ -10,18 +10,21 @@ cd $tmpdir
 # Test old syntax
 cat >Dockerfile <<EOF
 # labelprefix=org.opensuse.nano
-PREFIXEDLABEL org.opencontainers.image.title="Example container"
+PREFIXEDLABEL org.opencontainers.image.title="Example container."
 PREFIXEDLABEL org.opencontainers.image.description="This contains nano"
+PREFIXEDLABEL test.whitespace="Two  spaces. One	tab."
 EOF
 
 export BUILD_DIST=
 sh "${script}"
 
 diff -u Dockerfile - <<EOF
-LABEL org.opensuse.nano.title="Example container"
-LABEL org.opencontainers.image.title="Example container"
+LABEL org.opensuse.nano.title="Example container."
+LABEL org.opencontainers.image.title="Example container."
 LABEL org.opensuse.nano.description="This contains nano"
 LABEL org.opencontainers.image.description="This contains nano"
+LABEL org.opensuse.nano.whitespace="Two  spaces. One	tab."
+LABEL test.whitespace="Two  spaces. One	tab."
 EOF
 
 rm -f Dockerfile
@@ -29,8 +32,9 @@ rm -f Dockerfile
 # Test new syntax
 cat >Dockerfile <<EOF
 # labelprefix=org.opensuse.nano
-LABEL org.opencontainers.image.title="Example container"
+LABEL org.opencontainers.image.title="Example container."
 LABEL org.opencontainers.image.description="This contains nano"
+LABEL test.whitespace="Two  spaces. One	tab."
 # endlabelprefix
 LABEL not.expanded.label="example"
 EOF
@@ -39,9 +43,11 @@ export BUILD_DIST=
 sh "${script}"
 
 diff -u Dockerfile - <<EOF
-LABEL org.opensuse.nano.title="Example container"
-LABEL org.opencontainers.image.title="Example container"
+LABEL org.opensuse.nano.title="Example container."
+LABEL org.opencontainers.image.title="Example container."
 LABEL org.opensuse.nano.description="This contains nano"
 LABEL org.opencontainers.image.description="This contains nano"
+LABEL org.opensuse.nano.whitespace="Two  spaces. One	tab."
+LABEL test.whitespace="Two  spaces. One	tab."
 LABEL not.expanded.label="example"
 EOF
