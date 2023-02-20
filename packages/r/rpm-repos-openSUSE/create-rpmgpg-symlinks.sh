a9d892c0e6a33d2816fecd593a1d78e9c72047e7
Pharaoh_Atem <>
2023-02-20 16:46:50 +0000

    Update rpm-repos-openSUSE to rev 9 via SR 1066811
    https://build.opensuse.org/request/show/1066811
    by user Pharaoh_Atem + dimstar_suse
    - Refresh GPG key link for Tumbleweed and add 2022 key for Leap (boo#1199184)

--- packages/r/rpm-repos-openSUSE/create-rpmgpg-symlinks.sh
+++ packages/r/rpm-repos-openSUSE/create-rpmgpg-symlinks.sh
@@ -19,6 +19,7 @@ declare -A RPMGPGKEYS
 
 # List of GPG keys and common names
 RPMGPGKEYS["openSUSE"]="gpg-pubkey-3dbdc284-53674dd4.asc"
+RPMGPGKEYS["openSUSE-2022"]="gpg-pubkey-29b700a4-62b07e22.asc"
 RPMGPGKEYS["openSUSE-Backports"]="gpg-pubkey-65176565-61a0ee8f.asc"
 RPMGPGKEYS["SuSE-SLE-15"]="gpg-pubkey-39db7c82-5847eb1f.asc"
 RPMGPGKEYS["SuSE-SLE-15.3"]="gpg-pubkey-39db7c82-5847eb1f.asc"
