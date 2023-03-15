d3da7780ea82abf09805dc4c68004c2446873d13
vulyanov <>
2023-03-15 17:56:26 +0000

    Update virt-libguestfs-tools-container to rev 10 via SR 1072100
    https://build.opensuse.org/request/show/1072100
    by user vulyanov + dimstar_suse

--- packages/v/virt-libguestfs-tools-container/entrypoint.sh
+++ packages/v/virt-libguestfs-tools-container/entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/bash -xe
 
-#KubeVirt provides LIBGUESTFS_PATH via the pod environemnt.
-LIBGUESTFS_PATH=${LIBGUESTFS_PATH:-/tmp/guestfs}
-LIBGUESTFS_APPLIANCE=/appliance.tar.xz
-
-mkdir -p ${LIBGUESTFS_PATH}
-tar -Jxf ${LIBGUESTFS_APPLIANCE} -C ${LIBGUESTFS_PATH} --strip-components=1
-touch ${LIBGUESTFS_PATH}/done
-
 /bin/bash
