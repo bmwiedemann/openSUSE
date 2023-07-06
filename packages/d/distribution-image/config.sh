9504354903567ca1aae75707091250a95f0affb7
dirkmueller <>
2023-07-06 16:29:14 +0000

    Update distribution-image to version 2023 / rev 1 via SR 1097018
    https://build.opensuse.org/request/show/1097018
    by user dirkmueller + favogt_factory

---
+++ packages/d/distribution-image/config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: (c) 2022-2023 SUSE LLC
+
+set -euo pipefail
+
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+echo "Configure image: [$kiwi_iname]..."
+
+#============================================
+# Import repositories' keys if rpm is present
+#--------------------------------------------
+if command -v rpm > /dev/null; then
+    suseImportBuildKey
+fi
+
+
+
+#=======================================
+# Clean up after zypper if it is present
+#---------------------------------------
+if command -v zypper > /dev/null; then
+    zypper -n clean
+fi
+
+rm -rf /var/log/zypp
+
+exit 0
