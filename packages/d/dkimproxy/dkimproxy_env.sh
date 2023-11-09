d6086c4a4ac32f26efaee72baa2037c51f3191dd
varkoly <>
2023-11-09 20:34:49 +0000

    Update dkimproxy to version 1.4.1 / rev 24 via SR 1124333
    https://build.opensuse.org/request/show/1124333
    by user varkoly + anag+factory

--- packages/d/dkimproxy/dkimproxy_env.sh
+++ packages/d/dkimproxy/dkimproxy_env.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
   
 # extract configuration from /etc/sysconfig/dkimproxy and write
-# environment to /run/sysconfig/dkimproxy to be used by
+# environment to /run/dkimproxy/sysconfig to be used by
 # systemd unit files.
 
 if [ -r /etc/sysconfig/dkimproxy ]; then
 	cat /etc/sysconfig/dkimproxy > /run/dkimproxy/sysconfig
 fi
 
-echo "#Fully qualified hostanme" >> /run/dkimproxy/sysconfig
+echo "# Fully qualified hostname" >> /run/dkimproxy/sysconfig
 echo "FQHN=$( hostname -f )" >> /run/dkimproxy/sysconfig
 
 
