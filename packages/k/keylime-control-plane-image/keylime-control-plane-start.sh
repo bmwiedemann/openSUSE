62a491efd779dfa7290b42b6b1e52cc08815e59c
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2024-01-17 18:38:57 +0000

    update

--- packages/k/keylime-control-plane-image/keylime-control-plane-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Start the verifier and the registar in the correct order
-/usr/bin/keylime_verifier &
-# TODO fix the race condition
-sleep 2
-exec /usr/bin/keylime_registrar
