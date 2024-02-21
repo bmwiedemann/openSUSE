0d35d3ba491eae81891228ec19449d4c7a0395fc
rfrohl <>
2024-02-21 16:55:07 +0000

    Update kernel-source-longterm to version 6.6.17 / rev 1 via SR 1147314
    https://build.opensuse.org/request/show/1147314
    by user rfrohl + anag+factory
    please add kernel-longterm (6.6.17) to Factory
    
    known issue: KMPs not yet propetly supported

---
+++ packages/k/kernel-source-longterm/config.sh
@@ -0,0 +1,22 @@
+# The version of the main tarball to use
+SRCVERSION=6.6
+# variant of the kernel-source package, either empty or "-rt"
+VARIANT=-longterm
+# enable kernel module compression
+COMPRESS_MODULES="zstd"
+COMPRESS_VMLINUX="xz"
+# Compile binary devicetrees on master and stable branches.
+BUILD_DTBS="Yes"
+# Generate a _multibuild file
+MULTIBUILD="Yes"
+# Use new style livepatch package names
+LIVEPATCH=livepatch
+# buildservice projects to build the kernel against
+OBS_PROJECT=openSUSE:Factory
+# Bugzilla info
+BUGZILLA_SERVER="apibugzilla.suse.com"
+BUGZILLA_PRODUCT="openSUSE Tumbleweed"
+# build documentation in HTML format
+BUILD_HTML=Yes
+# build documentation in PDF format
+BUILD_PDF=No
