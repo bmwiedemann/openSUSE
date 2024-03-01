f149560ca34eeace79866f69161725d8bab2aa30
StevenK <>
2024-03-01 22:36:43 +0000

    Delete python-pydot3 via SR 1153743
    https://build.opensuse.org/request/show/1153743
    by user StevenK + dimstar_suse
    abandoned upstream, pydot now supports Python 3 anyway

--- packages/p/python-pydot3/example-rank.py
+++
@@ -1,7 +0,0 @@
-
-graph = pydot.Dot('graphname', graph_type='digraph') 
-subg = pydot.Subgraph('', rank='same') 
-subg.add_node(pydot.Node('a')) 
-graph.add_subgraph(subg) 
-subg.add_node(pydot.Node('b')) 
-subg.add_node(pydot.Node('c')) 
