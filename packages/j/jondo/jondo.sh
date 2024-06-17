27982169b19969c32e6431d0b7ed0ed633e9e669
Bernhard M. Wiedemann <bwiedemann+opensusegit@suse.de>
2024-06-17 19:19:35 +0000

    update 2024-06-17 19:19

--- packages/j/jondo/jondo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /usr/share/java && exec /usr/bin/java -jar JAP.jar \
-        -Djava.net.preferIPv4Stack=true \
-        -XX:-UsePerfData \
-        --hideUpdate --noSystemErrorLog
--- packages/m/makewhat/get_version_number.sh
+++
@@ -1,1 +0,0 @@
-get_date_version_string `find_newest_file *.*`
