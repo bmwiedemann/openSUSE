b0b51f6bc4e1c1cb8004a1b90b5e4b210f282f80
sbrabec <>
2024-07-02 16:17:14 +0000

    Delete translation-update via SR 1184322
    https://build.opensuse.org/request/show/1184322
    by user sbrabec + anag+factory
    Package got no update for years, as we regularly get new translations from the upstream. In Factory, there was a last consumer of translation updates from translation-update package: pan. Now pan got a new version with fresh translations from the upstream.
    
    Further existence of translation-update does not bring any benefit. We do not any post-freeze and post-release translation updates any more.

--- packages/t/translation-update/reset-translation-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tar xvf *.bz2
-find translation-update -type f -name '*.po' | xargs rm -v
-tar cvjf translation-update.tar.bz2 translation-update
-sed -i.bak 's|^\(%{_datadir}/locale-langpack\)|#\1|
-s|^\(%dir %{_datadir}/locale-langpack\)|#\1|' *.spec
-exit 0
