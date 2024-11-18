b72f4aa51e73c631e488ed5beb1b10f0a7c0c6ec
factory-maintainer <>
2024-11-18 19:02:14 +0000

    Update nvidia-open-driver-G06-signed to version 550.127.05 / rev 48 via SR 1224830
    https://build.opensuse.org/request/show/1224830
    by user factory-maintainer + anag+factory
    Automatic submission by obs-autosubmit

--- packages/n/nvidia-open-driver-G06-signed/kmp-trigger.sh
+++ packages/n/nvidia-open-driver-G06-signed/kmp-trigger.sh
@@ -1,6 +1,6 @@
 # trigger removal of driver modules with non-existing or wrong
 # firmware when (new) firmware gets installed
-test -e /sys/module/nvidia && \
- cat /sys/class/drm/card*/device/vendor | grep -vq 10de && \
- rmmod nvidia_drm nvidia_uvm nvidia_modeset video nvidia &> /dev/null
-
+if test -e /sys/module/nvidia \
+    && cat /sys/class/drm/card*/device/vendor | grep -vq 10de; then
+    rmmod nvidia_drm nvidia_uvm nvidia_modeset video nvidia &> /dev/null
+fi
