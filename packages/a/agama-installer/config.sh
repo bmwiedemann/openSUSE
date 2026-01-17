eccfde9fabf5725956163f4fbfdf32b99a820e2d
IGonzalezSosa <>
2026-01-17 13:51:31 +0000

    Update agama-installer to version 19.pre.0.0 / rev 23 via SR 1327362
    https://build.opensuse.org/request/show/1327362
    by user IGonzalezSosa + dimstar_suse
    - Version 19.pre
    
    - Hide the data sharing notification in the Firefox browser
      (bsc#1252961)
    
    - Added new boot option "live.net_config=1" to interactively
      configure network settings (gh#agama-project/agama#2923)
    
    - Version 18
    
    - Fixed creating the /LiveOS/.packages.json.gz file in SLE builds
      (gh#agama-project/agama#2771)
    
    - Dump some Live ISO root image data directly to the ISO image,
      added files:
      /LiveOS/.info - a copy of the /var/log/build/info file from
        the root image
      /LiveOS/.packages.gz - list of installed package (plain text,
        basically a `rpm -qa` dump)
      /LiveOS/.packages.json.gz - list of installed packages (JSON
        format, better suitable for processing by scripts or tools)
      (gh#agama-project/agama#2717)
    
    - live-root: update final EULAs with the June 2024 deliv

--- packages/a/agama-installer/config.sh
+++ packages/a/agama-installer/config.sh
@@ -61,7 +61,6 @@ systemctl enable agama-web-server.service
 systemctl enable agama-dbus-monitor.service
 systemctl enable agama-autoinstall.service
 systemctl enable agama-hostname.service
-systemctl enable agama-proxy-setup.service
 systemctl enable agama-certificate-issue.path
 systemctl enable agama-certificate-wait.service
 systemctl enable agama-cmdline-process.service
@@ -134,7 +133,7 @@ mkdir /etc/cmdline.d
 echo "root=live:LABEL=$label" >/etc/cmdline.d/10-liveroot.conf
 echo "root_disk=live:LABEL=$label" >>/etc/cmdline.d/10-liveroot.conf
 echo 'install_items+=" /etc/cmdline.d/10-liveroot.conf "' >/etc/dracut.conf.d/10-liveroot-file.conf
-echo 'add_dracutmodules+=" dracut-menu agama-cmdline agama-dud live-self-update "' >>/etc/dracut.conf.d/10-liveroot-file.conf
+echo 'add_dracutmodules+=" dracut-menu agama-cmdline agama-dud live-self-update initrd-nmtui "' >>/etc/dracut.conf.d/10-liveroot-file.conf
 
 # decrease the kernel logging on the console, use a dracut module to do it early in the boot process
 echo 'add_dracutmodules+=" agama-logging "' > /etc/dracut.conf.d/10-agama-logging.conf
